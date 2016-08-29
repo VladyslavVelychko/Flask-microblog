1c9f7fd67f481efaafb532f5d9512bc09dedbf89
VladyslavVelychko <velyk95@gmail.com>
2016-08-29 22:27:59 +0300

    Add simple template

--- app/views.py
+++ app/views.py
@@ -3,4 +3,14 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello, world!"
+	user={'nickname': 'Vladyslav'}
+	return '''
+<html>
+	<head>
+		<title>My Flask microblog</title>
+	</head>
+	<body>
+		<h1>Hello, ''' +user['nickname'] + '''</h1>
+	</body>
+</html>
+'''
