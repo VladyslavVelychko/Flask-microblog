885fb0458f9e0698a8d8b817f2d3a7ce6cc35193
VladyslavVelychko <velyk95@gmail.com>
2016-08-29 22:50:14 +0300

    Add another template

--- app/views.py
+++ app/views.py
@@ -1,16 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
 	user={'nickname': 'Vladyslav'}
-	return '''
-<html>
-	<head>
-		<title>My Flask microblog</title>
-	</head>
-	<body>
-		<h1>Hello, ''' +user['nickname'] + '''</h1>
-	</body>
-</html>
-'''
+	return render_template('index.html', title='Home', user=user)
