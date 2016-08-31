d7f0d2302c964d8b1b91e9aeda6164af80472246
VladyslavVelychko <velyk95@gmail.com>
2016-08-31 23:27:29 +0300

    Added form view function

--- app/views.py
+++ app/views.py
@@ -1,20 +1,24 @@
 from flask import render_template
 from app import app
+from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-	user={'nickname': 'Vladyslav'}
-	posts=[
-		{
-		    'author': {'nickname': 'Andrey'},
-		    'body': 'Beautiful morning in Kyiv'
-
-		},
-		{
-		    'author': {'nickname': 'Marine'},
-		    'body': 'It was a great summer. See you soon, NYC!'
-
-}
-	]
+	user = {'nickname': 'Vladyslav'}
+	posts = [
+        {
+            'author': {'nickname': 'Andrey'},
+            'body': 'Beautiful day in Kyiv!'
+        },
+        {
+            'author': {'nickname': 'Marine'},
+            'body': 'It was a great summer. Thanks and see you soon, NYC!'
+        }
+    ]
 	return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	form=LoginForm()
+	return render_template('login.html', title='Sign In', form=form)
